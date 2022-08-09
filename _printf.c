19f99a267319ca2e7838c7a85bae397a3bee8241
jojomutanu003 <musalumutanu@gmail.com>
2022-08-09 13:22:46 -0700

    second test

--- _printf.c
+++ _printf.c
@@ -1,60 +1,87 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+
+ *_printf - function to print anything
+
+ *@format: types of argument passed to the function
+
+ *
+
+ *Return: number of cs printed
+
  */
 
 int _printf(const char *format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
+{
+
+	int check = 0, i;
+
+	va_list params;
+
+	int (*func)(va_list);
+
+
+
+	va_start(params, format);
+
+	if (!format)
+
 		return (-1);
 
-	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+
+	for (i = 0; format[i]; i++)
+
 	{
-		if (format[i] != '%')
+
+		if (format[i] == '%')
+
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
+
+			i++;
+
+			if (!(format[i]))
+
 				return (-1);
-			printed_chars += printed;
-		}									}
-	print_buffer(buffer, &buff_ind);
-	va_end(list);
-	return (printed_chars);
-}
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
+
+
+
+			func = get_func(format[i]);
+
+			if (!func)
+
+			{
+
+				_putchar('%');
+
+				_putchar(format[i]);
+
+				check += 2;
+
+			}
+
+			else
+
+				check += func(params);
+
+		}
+
+		else
+
+		{
+
+			_putchar(format[i]);
+
+			check++;
+
+		}
+
+	}
+
+	va_end(params);
+
+	return (check);
+
 }
