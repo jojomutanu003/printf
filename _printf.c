4e4fa1b62a77b5af8b05523cc50bd2e0b961c0c0
jojomutanu003 <musalumutanu@gmail.com>
2022-08-10 14:12:36 -0700

    we go again

--- _printf.c
+++ _printf.c
@@ -1,43 +1,21 @@
 #include "main.h"
 
-
-
 /**
-
- * _printf - function like printf
-
- * @format: string with format to print
-
- *
-
- * Return: number of chars that print
-
- */
+ *_printf - function like printf
+ *@format: string with format to print
+ *Return: number of chars that print
+*/
 
 int _printf(const char *format, ...)
-
 {
-
 	va_list args;
-
 	int length = 0;
 
-
-
 	if (format == NULL)
-
-		return (-1);
-
-
-
+	return (-1);
 	va_start(args, format);
-
-
-
 	length = _print_format(format, args);
-
 	va_end(args);
 
 	return (length);
-
 }
